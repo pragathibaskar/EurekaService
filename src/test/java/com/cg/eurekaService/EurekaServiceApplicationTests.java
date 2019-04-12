61e00224cbb6ee82197c55649172b271b6ffcfd2
pragathibaskar <47776430+pragathibaskar@users.noreply.github.com>
2019-04-12 11:21:06 +0530

    Update EurekaServiceApplicationTests.java

--- src/test/java/com/cg/eurekaService/EurekaServiceApplicationTests.java
+++ src/test/java/com/cg/eurekaService/EurekaServiceApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class EurekaServiceApplicationTests {
-        //demo
+        //demo for webhooks
 	@Test
 	public void contextLoads() {
 	}
